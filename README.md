8073a97746c59ab82f83413f96cf833eb0357636
Paris Mollo <parismollochristondis@gmail.com>
2021-11-01 23:25:59 +0100

    Merge sort fixed

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Paris Mollo - L2 Informatique Groupe 3
 # TD6 Algo
 ## Exercice 5: Tri Fusion
 
-* 5.1 []
+* 5.1 [x]
 In computer science, merge sort (also commonly spelled as mergesort) is an efficient, general-purpose, and **comparison-based** sorting algorithm. Merge sort is a divide and conquer algorithm that was invented by John von Neumann in 1945
 
 ```java
@@ -11,4 +11,4 @@ public static int[] fusion(int[] arr1, int[] arr2):
 	return sorted(arr1+arr2);
 ```
 
-* 5.2 []
+* 5.2 [] - Doute.
--- TriFusion.java
+++ TriFusion.java
@@ -1,56 +1,77 @@
 import java.util.Arrays;
 
 public class TriFusion {
-	private int arr3[];
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 
-		int arr1[] = {1, 4};
-		int arr2[] = {2, 5};
+		int[] input = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+		// System.out.println("copy_array(input): "+Arrays.toString(copy_array(input, 0, (input.length/2)-1)));
+		TriFusion tf  = new TriFusion();
+		int[] output = tf.triFusion(input);
+		System.out.println("Input: "+Arrays.toString(input));
+		System.out.println("Output: "+Arrays.toString(output));
 
-		TriFusion tri_fusion  = new TriFusion();
-		tri_fusion.fusion(arr1, arr2);
-		tri_fusion.results();
 	}
 
-	private void create_arr3(int arr1[], int arr2[]) {
-		this.arr3 = new int[arr1.length+arr2.length];
-		System.out.println("Array of size: "+this.arr3.length+ " created.");
+	private int[] create_arr3(int[] arr1, int[] arr2) {
+ 		return new int[arr1.length+arr2.length];
 	}
 
-	public void fusion(int arr1[], int arr2[]) {
-		create_arr3(arr1, arr2);
-		System.out.println("Merge sort of: "+Arrays.toString(arr1)+" and "+Arrays.toString(arr2));
+	private int[] fusion(int[] arr1, int[] arr2) {
+		int[] arr3 = create_arr3(arr1, arr2);
+		// System.out.println("Merge of: "+Arrays.toString(arr1)+" and "+Arrays.toString(arr2));
 		int i = 0; // current element arr1
 		int j = 0; // current element arr2
 		int y = 0; // current element arr3
 
 		while (i<arr1.length && j<arr2.length) {
 			int min = min(arr1[i], arr2[j]);
-			this.arr3[y] = min;
+			arr3[y] = min;
 			y++;
 			if(min == arr1[i]) {i++;}
 			if(min == arr2[j]) {j++;}
 		}
 
 		while (i<arr1.length) {
-			this.arr3[y] = arr1[i];
+			arr3[y] = arr1[i];
 			y++;
 			i++;
 		}
 
 		while (j<arr2.length) {
-			this.arr3[y] = arr2[j];
+			arr3[y] = arr2[j];
 			y++;
 			j++;
 		}
+		return arr3;
+	}
+
+	public int[] triFusion(int[] arr) {
+		int n = arr.length - 1;
+		if (arr.length == 1) {return arr;}
+
+		int[] arr1 = copy_array(arr, 0, n/2);
+		int[] arr2 = copy_array(arr, (n/2) + 1, n);
+
+		arr1 = triFusion(arr1);
+		arr2 = triFusion(arr2);
+
+		return fusion(arr1, arr2);
 	}
 
 	private int min(int a, int b) {
 		return (a < b) ? a : b ;
 	}
 
-	public void results() {
-		System.out.println(Arrays.toString(this.arr3));
+	private int[] copy_array(int[] array, int start, int end) {
+		// array = [10, 5, 20, 3], start = 0, end = 1
+		// copy_array(array, 0, 1) -> [10, 5]
+		int[] res = new int[(end-start) + 1];
+		int pointer = start;
+		for(int i = 0; i < res.length; i++) {
+			res[i] = array[pointer];
+			pointer++;
+		}
+		return res;
 	}
 }
